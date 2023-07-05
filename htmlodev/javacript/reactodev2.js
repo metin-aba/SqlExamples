b924732e34ca73d94e559a73d8404b61566ba8e7
Metinaba1905 <abametin722@gmail.com>
2023-07-05 18:18:23 +0300

    odev 2 eklendşi

---
+++ htmlodev/javacript/reactodev2.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+import React from 'react';
+import Header from './components/Header/Header';
+import Features from './components/Features/Features';
+import Testimonials from './components/Testimonials/Testimonials';
+import Download from './components/Download/Download';
+import Footer from './components/Footer/Footer';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Features />
+      <Testimonials />
+      <Download />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+import React from 'react';
+import Header from './components/Header/Header';
+import Features from './components/Features/Features';
+import Testimonials from './components/Testimonials/Testimonials';
+import Download from './components/Download/Download';
+import Footer from './components/Footer/Footer';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Features />
+      <Testimonials />
+      <Download />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
