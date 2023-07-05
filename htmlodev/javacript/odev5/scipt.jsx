f47fb9b729bc5bc17d0a0b5b6a041f13d1f601e3
Metinaba1905 <abametin722@gmail.com>
2023-07-05 18:24:58 +0300

    odev 5 eklendi

---
+++ htmlodev/javacript/odev5/scipt.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from './Button';
+
+const App = () => {
+  return (
+    <div>
+      <Button type="primary">Primary Button</Button>
+      <Button type="default">Default Button</Button>
+      <Button type="dashed">Dashed Button</Button>
+      <Button type="text">Text Button</Button>
+      <Button type="link">Link Button</Button>
+    </div>
+  );
+};
+
+export default App;
